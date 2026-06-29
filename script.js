247b4d214013235602c6b78de19d1743340e51eb
Hitantsoa Alexandre Fortunat <fortunathitantsoa@gmail.com>
2026-06-29 03:06:03 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(panel => {
+    panel.addEventListener("click", () => {
+        panel.classList.toggle("open");
+    })
+    
+})
+
+panels.forEach(panel => {
+    panel.addEventListener("transitionend", (e) => {
+        console.log(e.propertyName);
+        if (e.propertyName.includes("flex")) 
+        {
+             panel.classList.toggle("open-active");
+        }
+       
+    })
+    
+})
+
